b2c49d9577f6e867d611928a841ce345b3ec450e
Ahmet <ahmt9417@gmail.com>
2021-11-16 20:48:24 +0300

    Create joining action

--- src/components/mission/Mission.js
+++ src/components/mission/Mission.js
@@ -1,10 +1,17 @@
-import { useSelector } from 'react-redux';
+/* eslint-disable no-unused-vars */
+import { useSelector, useDispatch } from 'react-redux';
 import { Container, Table, Button } from 'react-bootstrap';
+import { joinMission } from '../../redux/missions/missions';
 
 const Mission = () => {
+  const dispatch = useDispatch();
   const missions = useSelector((state) => state);
   const missionsArray = Object.entries(missions)[0][1];
 
+  const handleClick = (e) => {
+    const { id } = e.target.parentNode.parentNode;
+    dispatch(joinMission(id));
+  };
   return (
     <Container>
       <Table striped bordered hover>
@@ -18,14 +25,16 @@ const Mission = () => {
         </thead>
         <tbody>
           {missionsArray.map((item) => (
-            <tr key={item.mission_id}>
+            <tr key={item.mission_id} id={item.mission_id}>
               <td>{item.mission_name}</td>
               <td>{item.description}</td>
               <td className="align-items-center">
                 <Button variant="secondary">NOT A MEMBER</Button>
               </td>
               <td>
-                <Button variant="outline-secondary">Join Mission</Button>
+                <Button variant="outline-secondary" onClick={handleClick}>
+                  Join Mission
+                </Button>
               </td>
             </tr>
           ))}
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,9 +1,19 @@
 const LOAD_MISSIONS = 'spaceTravelerHub/missions/LOAD_MISSIONS';
+const JOIN_MISSION = 'spaceTravelerHub/missions/JOIN_MISSION';
 
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case JOIN_MISSION: {
+      const newState = state.map((mission) => {
+        if (mission.mission_id !== action.payload) {
+          return mission;
+        }
+        return { ...mission, reserved: true };
+      });
+      return newState;
+    }
     case LOAD_MISSIONS:
       return action.payload;
     default:
@@ -14,6 +24,11 @@ const reducer = (state = initialState, action) => {
 export const loadMissions = (apiState) => ({
   type: LOAD_MISSIONS,
   payload: apiState,
+});
+
+export const joinMission = (id) => ({
+  type: JOIN_MISSION,
+  payload: id,
 });
 
 export default reducer;
