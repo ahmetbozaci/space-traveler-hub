93b3686a6cdf8b2f19a47263a80d3f8a433b2158
Ahmet <ahmt9417@gmail.com>
2021-11-16 11:49:54 +0300

    Create mission table

--- src/components/mission/Mission.js
+++ src/components/mission/Mission.js
@@ -1,7 +1,29 @@
+/** @format */
+
 import React from 'react';
+import { Container, Table } from 'react-bootstrap';
 
 const Mission = () => (
-  <div> heloo bro </div>
+  <Container>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Thaicom</td>
+          <td>Lorem ipsummm</td>
+          <td>Active Member, NOT A MEMBER</td>
+          <td>Join Mission, Leave Mission</td>
+        </tr>
+      </tbody>
+    </Table>
+  </Container>
 );
 
 export default Mission;
