71fbbefd0bc2331c65a6b40d9d385ab4834ee46b
Ahmet <ahmt9417@gmail.com>
2021-11-17 17:22:23 +0300

    Add func if no joined mission

--- src/components/myprofile/MissionProfile.js
+++ src/components/myprofile/MissionProfile.js
@@ -1,19 +1,22 @@
-import { ListGroup } from 'react-bootstrap';
+import { Alert, ListGroup } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
 const MissionProfile = () => {
   const missions = useSelector((state) => state.mission);
-  const newArray = missions.filter((mission) => mission.reserved);
+  const filteredMission = missions.filter((mission) => mission.reserved);
   return (
     <div>
       <h3>My Missions</h3>
-      <ListGroup>
-        {newArray.map((mission) => (
-          <ListGroup.Item key={mission.mission_id} className="pb-4">
+      <ListGroup className="mt-2">
+        {filteredMission.map((mission) => (
+          <ListGroup.Item key={mission.mission_id} className="p-4">
             {mission.mission_name}
           </ListGroup.Item>
         ))}
       </ListGroup>
+      {filteredMission.length === 0 && (
+        <Alert variant="danger"><h3>No Missions Joined</h3></Alert>
+      )}
     </div>
   );
 };
