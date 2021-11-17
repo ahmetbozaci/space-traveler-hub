2ec9851ae5f4ced714b8e7b0a9c4c83a4b345a9c
Ahmet <ahmt9417@gmail.com>
2021-11-17 23:21:09 +0300

    Add leave button to profile page

--- src/components/myprofile/MissionProfile.js
+++ src/components/myprofile/MissionProfile.js
@@ -1,21 +1,35 @@
-import { Alert, ListGroup } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+/* eslint-disable no-unused-vars */
+import { Alert, ListGroup, Button } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { joinMission } from '../../redux/missions/missions';
 
 const MissionProfile = () => {
+  const dispatch = useDispatch();
   const missions = useSelector((state) => state.mission);
   const filteredMission = missions.filter((mission) => mission.reserved);
+
+  const handleClick = (e) => {
+    const { id } = e.target.id;
+    dispatch(joinMission(id));
+  };
+
   return (
     <div>
       <h3>My Missions</h3>
       <ListGroup className="mt-2">
         {filteredMission.map((mission) => (
-          <ListGroup.Item key={mission.mission_id} className="p-4">
+          <ListGroup.Item key={mission.mission_id} className="p-4 d-flex justify-content-between">
             {mission.mission_name}
+            <div>
+              <Button onClick={handleClick} id={mission.mission_id}>Leave Mission</Button>
+            </div>
           </ListGroup.Item>
         ))}
       </ListGroup>
       {filteredMission.length === 0 && (
-        <Alert variant="danger"><h3>No Missions Joined</h3></Alert>
+        <Alert variant="danger">
+          <h3>No Missions Joined</h3>
+        </Alert>
       )}
     </div>
   );
