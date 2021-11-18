b2b8e81a59a5b7415d90625f7a12eee0aa28280f
Ahmet <ahmt9417@gmail.com>
2021-11-18 13:52:21 +0300

    Fix navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,21 @@
+/* eslint-disable no-unused-vars */
+/**
+ * /* eslint-disable no-unused-vars
+ *
+ * @format
+ */
+
 import {
-  Navbar, Nav, Container, Offcanvas,
+  Navbar, Nav, Container, Offcanvas, NavLink,
 } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { Link } from 'react-router-dom';
 import planet from '../../planet.png';
 
 const Header = () => (
-
-  <Navbar bg="light" expand={false}>
-    <Container fluid>
-      <Navbar.Brand>
+  <Navbar expand="sm" className="p-3 my-3 mx-5 border-bottom border-2">
+    <Container>
+      <Navbar.Brand as={Link} to="/">
         <img
           src={planet}
           width="30"
@@ -18,29 +25,20 @@ const Header = () => (
         />
         Space Travelers Hub
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="offcanvasNavbar" />
-      <Navbar.Offcanvas
-        id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel"
-        placement="end"
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title id="offcanvasNavbarLabel">Space Travelers Hub</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav>
-            <Nav.Link as={Link} to="/">
-              Rockets
-            </Nav.Link>
-            <Nav.Link as={Link} to="/missions">
-              Missions
-            </Nav.Link>
-            <Nav.Link as={Link} to="/my-profile">
-              My Profile
-            </Nav.Link>
-          </Nav>
-        </Offcanvas.Body>
-      </Navbar.Offcanvas>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <NavbarCollapse id="responsive-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link as={Link} to="/">
+            Rockets
+          </Nav.Link>
+          <NavLink as={Link} to="/missions">
+            Missions
+          </NavLink>
+          <NavLink as={Link} to="/my-profile">
+            My Profile
+          </NavLink>
+        </Nav>
+      </NavbarCollapse>
     </Container>
   </Navbar>
 );
